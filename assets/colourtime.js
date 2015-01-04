a55b4f3cf461e2fe84737145dc1e654334dbcb2f
Blieque Mariguan <himself@blieque.co.uk>
2015-01-04 04:17:03 +0000

    better png sprite, js debugging
    I have replaced the PNG sprite with a better one, made with Photoshop.
    Yay for proprietary software, right?
    
    I have also added some debugging guff to the colourtime script so that
    if the "record" variable is set to true, the page will download hue,
    saturation and lightness data from changing-colour business, and
    refresh. Each download should be just over 3600 bytes. I intend to leave
    this running overnight, to collect a load of data. I'll then stick this
    into Excel or some open-source alternative to ensure it's "random"
    enough for my liking.

--- assets/colourtime.js
+++ assets/colourtime.js
@@ -17,6 +17,10 @@ var timeMs			= 0,
 	lightnessMult	= Math.floor(Math.random() * 10) + 10,
 	lightnessStrch	= 1 / (Math.random() + 1.5);
 
+var record			= window.location.hash == "#record",
+	recordInterval	= 2,
+	recordString	= "";
+
 // functions
 
 function updateColour() {
@@ -35,6 +39,23 @@ function updateColour() {
 	for (var i = 0; i < elements.length; i++) {
 		elements[i].style.cssText	= cssString;
 	};
+
+	if (record && hue % recordInterval == 0) {
+
+		recordString  += hue + " " + Math.round(saturation) + " " + Math.round(lightness) + ",";
+
+		if (recordString.length > 3600) {
+
+			dataUri		= "data:application/octet-stream," + encodeURIComponent(recordString);
+			dlWindow	= window.open(dataUri, "download");
+
+			var	hash	= window.location.hash == "#record" ? "" : "#record";
+			window.location	= window.location.href + hash;
+			window.location.reload();
+
+		}
+
+	}
 
 }
 
